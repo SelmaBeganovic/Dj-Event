a9d05459de2d85eb760215b051798b6e2988e537
nermin-hrustic <nermin.hrustic@saltsquare.io>
2022-02-18 15:28:27 +0100

    Create model

--- server/api/events/event_model.py
+++ server/api/events/event_model.py
@@ -6,4 +6,9 @@ class Event(db.Model):
     name = db.Column(db.String(128))
     venue = db.Column(db.String(128))
     address = db.Column(db.String(128))
-
+    slug = db.Column(db.String(128))
+    performers = db.Column(db.String(128))
+    date = db.Column(db.Date())
+    time = db.Column(db.String(20))
+    description = db.Column(db.String(250))
+    image = db.Column(db.String(250))
